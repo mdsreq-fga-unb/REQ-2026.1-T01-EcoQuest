b1c2cf0b976e2481656b1f6f8be1b6cab7b02e73
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-05-28 10:07:50 -0300

    feat(package): update package.json and tsconfig for React support and HTML rendering

--- server/src/index.tsx
+++ server/src/index.tsx
@@ -1,7 +1,20 @@
+import { Html, html } from "@elysia/html";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.use(html())
+	.get("/", () => (
+		<html lang="en">
+			<head>
+				<title>Hello World</title>
+			</head>
+			<body>
+				<h1>Hello World</h1>
+			</body>
+		</html>
+	))
+	.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
