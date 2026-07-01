e35be6fcf803a89deb42232f431e78d47dbf11e3
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-07-01 00:32:48 -0300

    fix: make PEV map page publicly accessible without auth

--- server/src/modules/localizar_pev/controller.tsx
+++ server/src/modules/localizar_pev/controller.tsx
@@ -3,16 +3,8 @@ import { Elysia } from "elysia";
 import { sessionPlugin } from "../../plugins/session";
 import { MapaView } from "./views";
 
-export const localizarPevController = new Elysia().use(sessionPlugin).get(
-	"/localizar-pev",
-	async ({ sessao, set }) => {
-		if (!sessao) {
-			set.status = 302;
-			set.headers["Location"] = "/auth/login";
-			return;
-		}
-
-		return <MapaView nomeUsuario={sessao.nome} />;
-	},
-	{ auth: true },
-);
+export const localizarPevController = new Elysia()
+	.use(sessionPlugin)
+	.get("/localizar-pev", async ({ sessao }) => {
+		return <MapaView nomeUsuario={sessao?.nome ?? "Visitante"} />;
+	});
